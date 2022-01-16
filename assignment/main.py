5b7024b5b2cdf34aaad0f030b8feed03fab92a9f
Han Sheng <hanshengliang@outlook.com>
2022-01-17 01:10:41 +0800

    :zap: Distribution Module

--- assignment/main.py
+++ assignment/main.py
@@ -22,6 +22,98 @@ def check_is_hospital_code_valid(hospital_code):
     return False
 
 
+def check_is_item_code_valid(supplier_code, item_code):
+    ppe_file_object = open("ppe.txt", "r")
+    for ppe_details in ppe_file_object:
+        ppe_details = ppe_details.rstrip()
+        ppe_details = ppe_details.split(",")
+        if ppe_details[0] == supplier_code and ppe_details[1] == item_code:
+            return True
+    return False
+
+
+def check_item_stock_is_enough(supplier_code, item_code, quantity_needed):
+    ppe_file_object = open("ppe.txt", "r")
+    for ppe_details in ppe_file_object:
+        ppe_details = ppe_details.rstrip()
+        ppe_details = ppe_details.split(",")
+        if ppe_details[0] == supplier_code and ppe_details[1] == item_code:
+            if int(ppe_details[2]) < int(quantity_needed):
+                return False
+            else:
+                return True
+
+
+def update_stock(supplier_code, item_code, new_quantity, action):
+
+    if action == "MINUS":
+        edited_item_list = []
+        initial_ppe_file_object = open("ppe.txt", "r")
+        temp_ppe_file_object = open("temp_ppe.txt", "w")
+
+        for initial_item_details in initial_ppe_file_object:
+            initial_item_details = initial_item_details.rstrip()
+            initial_item_details = initial_item_details.split(",")
+            if initial_item_details[0] == supplier_code and initial_item_details[1] == item_code:
+                edited_item_list.append({
+                    "supplier_code": initial_item_details[0],
+                    "item_code": initial_item_details[1],
+                    "item_quantity": str(int(initial_item_details[2]) - new_quantity)
+                })
+            else:
+                edited_item_list.append({
+                    "supplier_code": initial_item_details[0],
+                    "item_code": initial_item_details[1],
+                    "item_quantity": initial_item_details[2]
+                })
+        initial_ppe_file_object.close()
+
+        for edited_item_details in edited_item_list:
+            temp_ppe_file_object.write(
+                "{},{},{}".format(edited_item_details["supplier_code"], edited_item_details["item_code"],
+                                  edited_item_details["item_quantity"]))
+            temp_ppe_file_object.write("\n")
+        temp_ppe_file_object.close()
+        os.rename("temp_ppe.txt", "ppe.txt")
+    else:
+        initial_item_list = []
+        initial_ppe_file_object = open("ppe.txt", "r")
+        temp_ppe_file_object = open("temp_ppe.txt", "w")
+
+        for initial_item_details in initial_ppe_file_object:
+            initial_item_details = initial_item_details.rstrip()
+            initial_item_details = initial_item_details.split(",")
+            initial_item_list.append({
+                "supplier_code": initial_item_details[0],
+                "item_code": initial_item_details[1],
+                "item_quantity": initial_item_details[2]
+            })
+        initial_ppe_file_object.close()
+
+        edited_item_list = []
+
+        for initial_item_details in initial_item_list:
+            if initial_item_details["supplier_code"] == supplier_code and initial_item_details[
+                "item_code"] == item_code:
+                edited_item_list.append({
+                    "supplier_code": supplier_code,
+                    "item_code": item_code,
+                    "item_quantity": new_quantity
+                })
+            else:
+                edited_item_list.append({
+                    "supplier_code": initial_item_details["supplier_code"],
+                    "item_code": initial_item_details["item_code"],
+                    "item_quantity": initial_item_details["item_quantity"]
+                })
+
+        for edited_item_details in edited_item_list:
+            temp_ppe_file_object.write("{},{},{}".format(edited_item_details["supplier_code"], edited_item_details["item_code"], edited_item_details["item_quantity"]))
+            temp_ppe_file_object.write("\n")
+        temp_ppe_file_object.close()
+        os.rename("temp_ppe.txt", "ppe.txt")
+
+
 def supplier_login():
     is_valid_supplier = True
     while is_valid_supplier:
@@ -193,137 +285,83 @@ def add_item_to_inventory():
     ppe_file_object.close()
 
 
-def update_item_quantity():
-    initial_item_list = []
-    initial_ppe_file_object = open("ppe.txt", "r")
-    temp_ppe_file_object = open("temp_ppe.txt", "w")
-    update_item = True
-
-    for initial_item_details in initial_ppe_file_object:
-        initial_item_details = initial_item_details.rstrip()
-        initial_item_details = initial_item_details.split(",")
-        initial_item_list.append({
-            "supplier_code": initial_item_details[0],
-            "item_code": initial_item_details[1],
-            "item_quantity": initial_item_details[2]
-        })
-    initial_ppe_file_object.close()
-
-    while update_item:
-        edited_item_list = []
-        supplier_code = input("Enter Supplier Code: ")
-        if supplier_code == "":
-            print("Invalid Supplier Code")
-            continue
-        item_code = input("Enter Item Code: ")
-        if item_code == "":
-            print("Invalid Item Code")
-            continue
-        is_item_found = False
-        for initial_item_details in initial_item_list:
-            if initial_item_details["supplier_code"] == supplier_code and initial_item_details["item_code"] == item_code:
-                is_item_found = True
-                break
-        if not is_item_found:
-            print("Invalid Supplier Code or Item Code")
-            continue
-        item_quantity = input("Enter New Item Quantity: ")
-        if item_quantity == "":
-            print("Invalid Item Quantity")
-            continue
-        else:
-            quantity_is_string = True
-            while quantity_is_string:
-                try:
-                    item_quantity = int(item_quantity)
-                    quantity_is_string = False
-                except:
-                    print("Item Quantity must be a number")
-                    item_quantity = input("Enter New Item Quantity: ")
-        if item_code != "" and supplier_code != "" and item_quantity != "" and not quantity_is_string:
-            for initial_item_details in initial_item_list:
-                if initial_item_details["supplier_code"] == supplier_code and initial_item_details["item_code"] == item_code:
-                    edited_item_list.append({
-                        "supplier_code": supplier_code,
-                        "item_code": item_code,
-                        "item_quantity": item_quantity
-                    })
-                else:
-                    edited_item_list.append({
-                        "supplier_code": initial_item_details["supplier_code"],
-                        "item_code": initial_item_details["item_code"],
-                        "item_quantity": initial_item_details["item_quantity"]
-                    })
-        continue_ask_confirm = True
-        while continue_ask_confirm:
-            break_process = input("Want to edit next item? [0-no 1-yes]: ")
-            if break_process == "0" or break_process == "1":
-                if break_process == "0":
-                    continue_ask_confirm = False
-                    update_item = False
-                else:
-                    continue_ask_confirm = False
-                    update_item = True
-            else:
-                continue_ask_confirm = True
-                print("Invalid Input, only accept 0 and 1")
-    for edited_item_details in edited_item_list:
-        temp_ppe_file_object.write("{},{},{}".format(edited_item_details["supplier_code"], edited_item_details["item_code"], edited_item_details["item_quantity"]))
-        temp_ppe_file_object.write("\n")
-    temp_ppe_file_object.close()
-    os.rename("temp_ppe.txt", "ppe.txt")
-
-
 def distribution_module():
     distribution_file_object = open("distribution.txt", "a")
     distribute_item = True
     while distribute_item:
         supplier_code = input("Enter Supplier Code: ")
-        if supplier_code == "":
-            print("Invalid Supplier Code")
-            continue
-        else:
-            invalid_supplier = True
-            while invalid_supplier:
-                is_valid_supplier_code = check_is_supplier_code_valid(supplier_code)
-                if is_valid_supplier_code:
-                    invalid_supplier = False
+        while True:
+            if supplier_code == "":
+                print("Supplier Code cannot be empty")
+                supplier_code = input("Enter Supplier Code: ")
+            else:
+                is_valid_supplier = check_is_supplier_code_valid(supplier_code)
+                if is_valid_supplier:
                     break
                 print("Supplier not found")
                 supplier_code = input("Enter Supplier Code: ")
 
         target_hospital_code = input("Enter Target Hospital Code: ")
-        if target_hospital_code == "":
-            print("Invalid target Hospital Code")
-            continue
-        else:
-            invalid_hospital = True
-            while invalid_hospital:
+        while True:
+            if target_hospital_code == "":
+                print("Hospital Code cannot be empty")
+                target_hospital_code = input("Enter Target Hospital Code: ")
+            else:
                 is_valid_hospital_code = check_is_hospital_code_valid(target_hospital_code)
                 if is_valid_hospital_code:
-                    invalid_hospital = False
                     break
                 print("Hospital not found")
                 target_hospital_code = input("Enter Target Hospital Code: ")
 
-        quantity_to_distribute = input("Enter Quantity to Distribute: ")
-        if quantity_to_distribute == "":
-            print("Invalid Item Quantity")
-            continue
-        else:
-            quantity_is_string = True
-            while quantity_is_string:
-                try:
-                    item_quantity = int(quantity_to_distribute)
-                    quantity_is_string = False
-                except:
-                    print("Item Quantity must be a number")
-                    quantity_to_distribute = input("Enter New Item Quantity: ")
-        if supplier_code != "" and target_hospital_code != "" and quantity_to_distribute != "":
-            current_timestamp = datetime.datetime.now()
-            print(current_timestamp)
-            # distribution_file_object.write("{},{}".format())
+        item_code = input("Enter Item Code: ")
+        while True:
+            if item_code == "":
+                print("Item Code cannot be empty")
+                item_code = input("Enter Item Code: ")
+            else:
+                is_valid_item = check_is_item_code_valid(supplier_code, item_code)
+                if is_valid_item:
+                    break
+                print("Item not found")
+                item_code = input("Enter Item Code: ")
 
+        quantity_to_distribute = input("Enter Quantity to Distribute: ")
+        while True:
+            if quantity_to_distribute == "":
+                print("Quantity cannot be empty")
+                quantity_to_distribute = input("Enter Quantity to Distribute: ")
+            else:
+                try:
+                    quantity_to_distribute = int(quantity_to_distribute)
+                    break
+                except:
+                    print("Quantity must be a number")
+                    quantity_to_distribute = input("Enter Quantity to Distribute: ")
+
+        if check_item_stock_is_enough(supplier_code, item_code, quantity_to_distribute):
+            update_stock(supplier_code, item_code, quantity_to_distribute, "MINUS")
+            current_timestamp = datetime.datetime.now()
+            distribution_file_object.write(
+                "{},{},{},{},{}".format(current_timestamp, supplier_code, target_hospital_code, item_code,
+                                        quantity_to_distribute))
+            distribution_file_object.write("\n")
+        else:
+            print("No Record or Invalid Stock")
+
+        continue_ask_confirm = True
+        while continue_ask_confirm:
+            break_process = input("Want to distribute next item? [0-no 1-yes]: ")
+            if break_process == "0" or break_process == "1":
+                if break_process == "0":
+                    continue_ask_confirm = False
+                    distribute_item = False
+                else:
+                    continue_ask_confirm = False
+                    distribute_item = True
+            else:
+                continue_ask_confirm = True
+                print("Invalid Input, only accept 0 and 1")
+    distribution_file_object.close()
 
 if __name__ == "__main__":
     distribution_module()
